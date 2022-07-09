753a3a1fc61a1bfed6fef26fbf862bc36cf52276
Kauã Vinícius <81399319+kaua-pt@users.noreply.github.com>
2022-07-09 14:49:33 -0300

    Inicio do trabalho com MYSQL

---
+++ SistemaDeAcademia/src/BancoDeDados/BaseDeDados.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package BancoDeDados;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet; 
+import java.sql.DriverManager;
+
+/**
+ * @author PC
+ *
+ */
+public class BaseDeDados {
+	
+	private Connection coneccao = null;
+	private Statement estado = null;
+	private ResultSet resultado = null;
+	
+	
+	public void conectar() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			this.coneccao = DriverManager.getConnection("jdbc:mysql://localhost:3306/trabalhoOO","root","tocarviolao3");
+			this.estado = this.coneccao.createStatement();
+			System.out.print("deu bom");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean estaConectado() {
+		if (coneccao != null) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void inserirElemento() {
+		try {
+			String query = "INSERT INTO aluno(nome,id,altura,idade,peso)";
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ SistemaDeAcademia/src/BancoDeDados/Test.java
@@ -0,0 +1,10 @@
+package BancoDeDados;
+
+public class Test {
+
+	public static void main(String[] args) {
+		BaseDeDados  db = new BaseDeDados();
+		db.conectar();
+	}
+
+}
